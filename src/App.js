26c7f7204cd8a8999936bcbf8605e8ec6ec30b17
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-09-27 16:46:37 -0400

    Add login navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import MainHeader from './components/MainHeader/MainHeader';
 import Login from './components/Login/Login';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const loginHandler = (email, password) => {
+    setIsLoggedIn(true);
+  }
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  }
+
   return (
     <Fragment>
       <MainHeader />
-      <Login />
+      <main>
+        { !isLoggedIn && <Login onLogin={loginHandler} />}
+        { isLoggedIn && <Home onLogout={logoutHandler} /> }
+      </main>
     </Fragment>
   );
 }
