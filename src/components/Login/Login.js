b4b25fb564df4441ad98bc18a4b67c5752c5a110
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-09-30 10:37:06 -0400

    add cleanup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,18 @@ const Login = (props) => {
 	const [formIsValid, setFormIsValid] = useState(false);
 
 	useEffect(()=> {
-		setFormIsValid(
-			enteredEmail.includes('@') && enteredPassword.trim().length > 6
-		);
+		const identifier = setTimeout(() => {
+			console.log('Checking Form Validation!');
+			setFormIsValid(
+				enteredEmail.includes('@') && enteredPassword.trim().length > 6
+			);
+		}, 500);
+
+		return () => {
+			console.log('CLEAN UP ' + enteredEmail);
+			clearTimeout(identifier);
+		};
+
 	}, [enteredEmail, enteredPassword]);
 
 	const emailChangeHandler = (event) => {
